cf5c34c2da444938826b9e4eb371c8cab9763212
Yassin DAHMOUNI <yassindahmouni34@gmail.com>
2022-06-12 19:53:09 +0200

    Apply terraform state locking

--- create-infrastructure/main.tf
+++ create-infrastructure/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "product-hunting-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "product-hunting-terraform-state-lock"
+  }
+}
+
 module "ecr" {
   source = "./ecr"
 }
