7090e7c8829b4cb289bd63e355173838797c9591
Yassin DAHMOUNI <yassindahmouni34@gmail.com>
2022-06-12 18:16:06 +0200

    Add DynamoDB table creation with Terraform

--- create-infrastructure/state-lock/main.tf
+++ create-infrastructure/state-lock/main.tf
@@ -17,3 +17,18 @@ resource "aws_s3_bucket_versioning" "product-hunting-s3-bucket-versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_dynamodb_table" "product-hunting-dynamodb-table" {
+  name           = "product-hunting-terraform-state-locking"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "product-hunting-terraform-state-locking"
+  }
+}
