ec6de1823c2b80b129bd02181d0da681f9d30817
Yassin DAHMOUNI <yassindahmouni34@gmail.com>
2022-06-28 18:29:27 +0200

    Edit database variable name inside 'db.js' api file

--- application/backend/api/db.js
+++ application/backend/api/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE
+    database: process.env.POSTGRES_DB
 });
 
 module.exports = pool;
