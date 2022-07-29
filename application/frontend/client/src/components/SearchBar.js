d02b0b5270de57fde76bd1cc8621a9b8a2f51f61
Yassin DAHMOUNI <yassindahmouni34@gmail.com>
2022-07-29 18:39:55 +0200

    Make Search bar dynamic, instead of using a form

--- application/frontend/client/src/components/SearchBar.js
+++ application/frontend/client/src/components/SearchBar.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const SearchBar = ({callback}) => {
     const [innerValue, setInnerValue] = useState("");
-    const handleSubmit = e => {
-        e.preventDefault();
+
+    const refreshSearch = () => {
         callback(innerValue);
     };
 
+    //Refresh search when search bar value changes
+    useEffect(() => {
+        refreshSearch();
+    }, [innerValue]); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <div className="searchBar">
-            <form onSubmit={handleSubmit}>
                 <input type="text" name="search" className="form-control mt-5 mb-4" value={innerValue} onChange={(e) => setInnerValue(e.target.value)} placeholder="Search a product"/>
-            </form>
         </div>
-    )
-}
+    );
+};
 
 export default SearchBar;
